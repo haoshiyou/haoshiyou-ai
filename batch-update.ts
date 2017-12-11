fef902a5763ad3644d13e384e381367e0a3a9a57
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-12-11 00:06:14 -0800

    Split owner update from listing update

--- batch-update.ts
+++ batch-update.ts
@@ -1,8 +1,8 @@
 import {HsyListing} from "./loopbacksdk/models/HsyListing";
 import {HsyUser} from "./loopbacksdk/models/HsyUser";
 import {HsyExtractor} from "./extractor";
-
-const config = {
+// haoshiyou-server-dev.herokuapp.com
+let config = {
   "loopback": {
     "http://localhost:3000": {
       "__domain": {
@@ -47,7 +47,8 @@ async function main() {
       .catch((err) => {
         console.log(JSON.stringify(err));
       });
-  let toUpdate:HsyListing[] = [];
+  let listingToUpdate:HsyListing[] = [];
+  let ownerToUpdate:HsyUser[] = [];
   let errorListing: HsyListing[] = [];
   let updateCount = {
     phone :0,
@@ -55,9 +56,11 @@ async function main() {
     zipcode: 0,
     price: 0,
     email: 0,
+    fullAddr: 0,
   };
   let imageOnly:HsyListing[] = [];
   let claimed:HsyListing[] = [];
+  // console.log(`Result = ${JSON.stringify(result, null, '  ')}`);
   result[0].body.forEach(listObj => {
     let hsyListing:HsyListing = <HsyListing>listObj;
     if (hsyListing) {
@@ -70,46 +73,55 @@ async function main() {
         return;
       }
 
-      let phone = HsyExtractor.extractPhone(hsyListing.content, listObj);
+
       let city = HsyExtractor.extractCity(hsyListing.content, listObj);
+      let fullAddr = HsyExtractor.extractFullAddr(hsyListing.content, listObj);
       let zipcode = HsyExtractor.extractZipcode(hsyListing.content, listObj);
       let price = HsyExtractor.extractPrice(hsyListing.content, listObj);
       let email = HsyExtractor.extractEmail(hsyListing.content, listObj);
-      let flagDirty:boolean = false;
-      if (hsyListing.owner && phone) {
-        hsyListing.owner.contactPhone = phone;
-        flagDirty = true;
-        updateCount.phone++;
-      }
+      let phone = HsyExtractor.extractPhone(hsyListing.content, listObj);
+
+      let dirtyListing = false;
+      let dirtyOwner = false;
       if (city) {
         hsyListing.addressCity = city;
-        flagDirty = true;
         updateCount.city++;
-
+        dirtyListing = true;
       }
       if (zipcode) {
         hsyListing.addressZipcode = zipcode;
-        flagDirty = true;
         updateCount.zipcode++;
+        dirtyListing = true;
+      }
+
+      if (fullAddr) {
+        hsyListing.addressLine = price;
+        updateCount.fullAddr++;
+        dirtyListing = true;
       }
       if (price) {
         hsyListing.price = price;
-        flagDirty = true;
         updateCount.price++;
+        dirtyListing = true;
+      }
+      if (hsyListing.owner && phone) {
+        hsyListing.owner.contactPhone = phone;
+        updateCount.phone++;
+        dirtyOwner = true;
       }
       if (hsyListing.owner && email) {
         hsyListing.owner.contactEmail = email;
-        flagDirty = true;
         updateCount.email++;
+        dirtyOwner = true;
       }
-      if (flagDirty) {
-        toUpdate.push(hsyListing);
-      }
+      if (dirtyListing) listingToUpdate.push(hsyListing);
+      if (dirtyOwner) ownerToUpdate.push(hsyListing.owner);
     }
   });
 
   console.log(`Total: ${result[0].body.length}
-  To Update: ${toUpdate.length}
+  Listing To Update: ${listingToUpdate.length}
+  Owner To Update: ${ownerToUpdate.length}
   Erorr: ${errorListing.length}
   Claimed: ${claimed.length},
   Image Only: ${imageOnly.length},
@@ -119,19 +131,56 @@ async function main() {
     console.log(`Warning: real update!`);
     let success = 0;
     let failure = 0;
-    let processes = toUpdate.map(l => loopback.put('HsyListings')
+    let successOwner = 0;
+    let failureOwner = 0;
+    let listingProcesses = listingToUpdate.map(async l =>
+        loopback.put('HsyListings')
         .json(l)
         .request()
-        .then(l => {
-          success ++;
+        .then(result => {
+          let l = result[0].body;
+          if(!l || !l.uid) {
+            // console.log(`Updated failure result = ${JSON.stringify(result, null, '  ')}`);
+            failure ++;
+          }
+          else {
+            // console.log(`Updated uid = ${l.uid}`);
+            success ++;
+          }
           return l;
         })
         .catch(e => {
           failure ++;
+          console.log(e);
           return e;
-        }));
-    let result = await Promise.all(processes);
-    console.log(`Succes ${success}, Fialure ${failure}`);
+        })
+    );
+
+    let ownerProcesses = ownerToUpdate.map(async u =>
+        loopback.put('HsyUsers')
+            .json(u)
+            .request()
+            .then(result => {
+              let u = result[0].body;
+              if(!u || !u.id) {
+                // console.log(`Updated failure result = ${JSON.stringify(result, null, '  ')}`);
+                failureOwner ++;
+              }
+              else {
+                // console.log(`Updated user.id = ${u.id}`);
+                successOwner ++;
+              }
+              return u;
+            })
+            .catch(e => {
+              failureOwner ++;
+              console.log(e);
+              return e;
+            })
+    );
+    await Promise.all(listingProcesses.concat(ownerProcesses));
+    console.log(`Success ${success}, Failure ${failure}`);
+    console.log(`SuccessOwner ${successOwner}, FailureOwner ${failureOwner}`);
   } else {
     console.log(`Not real run, just for fun!`);
   }
